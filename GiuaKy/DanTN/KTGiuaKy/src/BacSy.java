3d2efbf53da5f95923fe7fd79d4734c1eea11374
Trần Ngọc Dân <42628564+NGOCDAN002@users.noreply.github.com>
2019-04-11 09:41:57 +0700

    fix kiem tra giua ky

---
+++ GiuaKy/DanTN/KTGiuaKy/src/BacSy.java
@@ -0,0 +1,65 @@
+﻿public class BacSy{
+	private String ten;
+	private String gioiTinh;
+	private String maBacSy;
+	private String khoa;
+	private String ngayBatDauVaoLam;
+	private double tuoi;
+	private double cmnd;
+	private double luong;
+	private double phuCap;
+	public String getTen(){
+		return ten;
+	}
+	public void setTen(String ten){
+		this.ten = ten;
+	}
+	public String getGioiTinh(){
+		return gioiTinh;
+	}
+	public void setGioiTinh(String gioiTinh){
+		this.gioiTinh = gioiTinh;
+	}
+	public String getMaBacSy(){
+		return maBacSy;
+	}
+	public void setMaBacSy(String maBacSy){
+		this.maBacSy = maBacSy;
+	}
+	public String getKhoa(){
+		return khoa;
+	}
+	public void setKhoa(String khoa){
+		this.khoa = khoa;
+	}
+	public String getNgayBatDauVaoLam(){
+		return ngayBatDauVaoLam;
+	}
+	public void setNgayBatDauVaoLam(String ngayBatDauVaoLam){
+		this.ngayBatDauVaoLam = ngayBatDauVaoLam;
+	}
+	public double getTuoi(){
+		return tuoi;
+	}
+	public void setTuoi(double tuoi){
+		this.tuoi = tuoi;
+	}
+	public double getCMND(){
+		return cmnd;
+	}
+	public void setCMND(double cmnd){
+		this.cmnd = cmnd;
+	}
+	public double getLuong(){
+		return luong;
+	}
+	public void setLuong(double luong){
+		this.luong = luong;
+	}
+	public double getPhuCap(){
+		return phuCap;
+	}
+	public void setPhuCap(double phuCap){
+		this.phuCap = phuCap;
+	}
+}
