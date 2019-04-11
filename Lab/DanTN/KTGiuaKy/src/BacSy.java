353d7acba79523d787b0bb87f3e0226b98d5c36a
Trần Ngọc Dân <42628564+NGOCDAN002@users.noreply.github.com>
2019-04-11 08:54:23 +0700

    KT Giua Ky

---
+++ Lab/DanTN/KTGiuaKy/src/BacSy.java
@@ -0,0 +1,58 @@
+﻿public class BacSy{
+	private String ten, gioiTinh, maBacSy, khoa, ngayBatDauVaoLam;
+	private double tuoi, cmnd, luong, phuCap;
+	public String getTen(){
+		return ten;
+	}
+	public void setTen(String ten){
+		this.ten = ten;
+	}
+	public String getGioiTinh(){
+		return gioiTinh;
+	}
+	public void setGioiTinh(String gioiTinh){
+		this.gioiTinh = gioiTinh;
+	}
+	public String getMaBacSy(){
+		return maBacSy;
+	}
+	public void setMaBacSy(String maBacSy){
+		this.maBacSy = maBacSy;
+	}
+	public String getKhoa(){
+		return khoa;
+	}
+	public void setKhoa(String khoa){
+		this.khoa = khoa;
+	}
+	public String getNgayBatDauVaoLam(){
+		return ngayBatDauVaoLam;
+	}
+	public void setNgayBatDauVaoLam(String ngayBatDauVaoLam){
+		this.ngayBatDauVaoLam = ngayBatDauVaoLam;
+	}
+	public double getTuoi(){
+		return tuoi;
+	}
+	public void setTuoi(double tuoi){
+		this.tuoi = tuoi;
+	}
+	public double getCMND(){
+		return cmnd;
+	}
+	public void setCMND(double cmnd){
+		this.cmnd = cmnd;
+	}
+	public double getLuong(){
+		return luong;
+	}
+	public void setLuong(double luong){
+		this.luong = luong;
+	}
+	public double getPhuCap(){
+		return phuCap;
+	}
+	public void setPhuCap(double phuCap){
+		this.phuCap = phuCap;
+	}
+}
---
+++ Lab/DanTN/KTGiuaKy/src/BenhNhan.java
@@ -0,0 +1,64 @@
+﻿public class BenhNhan{
+	private String ten, gioiTinh, maBenhNhan, benh, phuongPhapChuaBenh, ngayNhapVien, ngayRaVien;
+	private double tuoi, cmnd, tienThanhToan;
+	public String getTen(){
+		return ten;
+	}
+	public void setTen(String ten){
+		this.ten = ten;
+	}
+	public String getGioiTinh(){
+		return gioiTinh;
+	}
+	public void setGioiTinh(String gioiTinh){
+		this.gioiTinh = gioiTinh;
+	}
+	public String getMaBenhNhan(){
+		return maBenhNhan;
+	}
+	public void setMaBenhNhan(String maBenhNhan){
+		this.maBenhNhan = maBenhNhan;
+	}
+	public String getBenh(){
+		return benh;
+	}
+	public void setBenh(String benh){
+		this.benh = benh;
+	}
+	public String getPhuongPhapChuaBenh(){
+		return phuongPhapChuaBenh;
+	}
+	public void setPhuongPhapChuaBenh(String phuongPhapChuaBenh){
+		this.phuongPhapChuaBenh = phuongPhapChuaBenh;
+	}
+	public String getNgayNhapVien(){
+		return ngayNhapVien;
+	}
+	public void setNgayNhapVien(String ngayNhapVien){
+		this.ngayNhapVien = ngayNhapVien;
+	}
+	public String getNgayRaVien(){
+		return ngayRaVien;
+	}
+	public void setNgayRaVien(String ngayRaVien){
+		this.ngayRaVien = ngayRaVien;
+	}
+	public double getTuoi(){
+		return tuoi;
+	}
+	public void setTuoi(double tuoi){
+		this.tuoi = tuoi;
+	}
+	public double getCMND(){
+		return cmnd;
+	}
+	public void setCMND(double cmnd){
+		this.cmnd = cmnd;
+	}
+	public double getTienThanhToan(){
+		return tienThanhToan;
+	}
+	public void setTienThanhToan(double tienThanhToan){
+		this.tienThanhToan = tienThanhToan;
+	}
+}
