41bf49771c3e159d7d19506ef082d46ff2cb9a85
hungtruong1403 <hungtruong14032000@gmail.com>
2019-04-12 08:43:06 +0700

    Assignment4

---
+++ Assignment/PhucHung/assignment4/src/model/bean/BienLai.java
@@ -0,0 +1,36 @@
+package model.bean;
+
+public class BienLai {
+	private String maCongTo;
+	private double chiSoCu;
+	private double chiSoMoi;
+
+	public String getMaCongTo() {
+		return maCongTo;
+	}
+
+	public void setMaCongTo(String maCongTo) {
+		this.maCongTo = maCongTo;
+	}
+
+	public double getChiSoCu() {
+		return chiSoCu;
+	}
+
+	public void setChiSoCu(double chiSoCu) {
+		this.chiSoCu = chiSoCu;
+	}
+
+	public double getChiSoMoi() {
+		return chiSoMoi;
+	}
+
+	public void setChiSoMoi(double chiSoMoi) {
+		this.chiSoMoi = chiSoMoi;
+	}
+
+	public double getSoTienPhaiTra() {
+		return (chiSoMoi - chiSoCu) * 750;
+	}
+	
+}
