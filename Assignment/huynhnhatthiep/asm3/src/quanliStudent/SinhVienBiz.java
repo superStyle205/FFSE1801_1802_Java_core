cfd44998c286ef1675e3e44235c2179a5501ce01
huynhnhatthiep <huynhnhatthiep98@gmail.com>
2019-04-12 11:09:55 +0700

    asm3

---
+++ Assignment/huynhnhatthiep/asm3/src/quanliStudent/SinhVienBiz.java
@@ -0,0 +1,25 @@
+package quanliStudent;
+
+public class SinhVienBiz extends SinhVienFpt {
+	private double diemMarketing;
+	private double diemSales;
+
+	public SinhVienBiz(String hoTen, String nganh, double diemMarketing, double diemSales) {
+		super(hoTen, nganh);
+		this.diemMarketing = diemMarketing;
+		this.diemSales = diemSales;
+	}
+
+	@Override
+	public double getDiem() {
+		return (diemMarketing * 2 + diemSales) / 3;
+	}
+
+	@Override
+	public void xuat() {
+		super.xuat();
+		System.out.println("diem marketing : " + diemMarketing);
+		System.out.println("diem sales : " + diemSales);
+	}
+
+}
