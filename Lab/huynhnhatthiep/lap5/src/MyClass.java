dbf03fffa282ab7e296822da4111ad75ae7e23c2
huynhnhatthiep <huynhnhatthiep98@gmail.com>
2019-04-12 11:07:41 +0700

    lap5

---
+++ Lab/huynhnhatthiep/lap5/src/MyClass.java
@@ -0,0 +1,8 @@
+import controller.ManagerPersonController;
+public class MyClass {
+		public static void main(String... args) {
+			ManagerPersonController managerPersonController = new ManagerPersonController();
+			managerPersonController.loadProgram();
+		}
+
+}
