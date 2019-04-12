696befdd65367c1a1de05521791227368750f9d0
hungtruong1403 <hungtruong14032000@gmail.com>
2019-04-12 08:53:44 +0700

    Lap1

---
+++ Lab/PhucHung/Lap1/src/Bai3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bai3 {
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.print("Moi ban nhap canh : ");
+    	int canh = sc.nextInt();
+    	System.out.println(Math.pow(canh, 3));
+    	
+    }
+}
---
+++ Lab/PhucHung/Lap1/src/Bai5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Bai5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Moi ban nhap chieu dai : ");
+		int dai = sc.nextInt();
+
+		System.out.print("Moi ban nhap chieu rong :");
+		int rong = sc.nextInt();
+		double CV = (dai + rong) * 2;
+		double DT = dai * rong;
+		System.out.printf("Chu vi la:" + CV + "Dien tich la" + DT);
+	}
+}
