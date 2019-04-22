483077870f5be906bfcd45937742c9e7de04bd98
loctran1 <loctr9x@gmail.com>
2019-04-22 11:53:29 +0700

    add lab6

---
+++ Lab/locTran/Lab6/src/Lab6/MyClass.java
@@ -0,0 +1,13 @@
+package Lab6;
+
+public class MyClass {
+	public static void main(String[] args) throws Exception {
+
+		QuanLySinhVien qlsv = new QuanLySinhVien();
+
+		qlsv.loadDataDumy();
+
+		qlsv.menu();
+
+	}
+}
---
+++ Lab/locTran/Lab6/src/Lab6/SinhVien.java
@@ -0,0 +1,62 @@
+package Lab6;
+
+public class SinhVien {
+
+	private int id;
+	private String name;
+	private int age;
+	private String address;
+	private double gpa;
+
+	public SinhVien() {
+	}
+
+	public SinhVien(int id, String name, int age, String address, double gpa) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.address = address;
+		this.gpa = gpa;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+}
