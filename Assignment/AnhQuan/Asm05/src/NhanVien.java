1c5c8ac44eb8dc1ce9c5331553cd16ce6b12e262
Briefseverus <42628529+Arthurdoyle@users.noreply.github.com>
2019-05-02 09:12:51 +0700

    Update NhanVien.java

--- Assignment/AnhQuan/Asm05/src/NhanVien.java
+++ Assignment/AnhQuan/Asm05/src/NhanVien.java
@@ -3,12 +3,23 @@ public class NhanVien {
 String hoTen;
 String phongBan;
 String chucVu;
-
+double luong;
 int soNgayCong;
 double heSoLuong;
 int phuCap;
 public NhanVien() {
 	
+}
+public NhanVien(String hoTen, String phongBan, String chucVu, double luong, int soNgayCong, double heSoLuong,
+		int phuCap) {
+	super();
+	this.hoTen = hoTen;
+	this.phongBan = phongBan;
+	this.chucVu = chucVu;
+	this.luong = luong;
+	this.soNgayCong = soNgayCong;
+	this.heSoLuong = heSoLuong;
+	this.phuCap = phuCap;
 }
 public NhanVien(String hoTen, String phongBan, String chucVu, int soNgayCong, double heSoLuong, int phuCap) {
 	
@@ -18,6 +29,12 @@ public NhanVien(String hoTen, String phongBan, String chucVu, int soNgayCong, do
 	this.soNgayCong = soNgayCong;
 	this.heSoLuong = heSoLuong;
 	this.phuCap = phuCap;
+}
+public double getLuong() {
+	return luong;
+}
+public void setLuong(double luong) {
+	this.luong = luong;
 }
 public String getHoTen() {
 	return hoTen;
