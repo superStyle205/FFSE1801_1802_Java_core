5c409ae0de86d5539cd53c1b1f76a25111427b70
superStyle205 <super.style205@gmail.com>
2019-06-14 12:06:15 +0700

    add tab

--- ftCafeProject/ftCoffee/src/ft/coffee/view/component/jtabbedpane/TabContentPanel.java
+++ ftCafeProject/ftCoffee/src/ft/coffee/view/component/jtabbedpane/TabContentPanel.java
@@ -1,5 +1,14 @@
 package ft.coffee.view.component.jtabbedpane;
 
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
@@ -7,15 +16,71 @@ public class TabContentPanel extends JTabbedPane {
 
 	private static final long serialVersionUID = 1L;
 
+	private List<String> listTabName;
+
 	public TabContentPanel() {
 		setTabLayoutPolicy(SCROLL_TAB_LAYOUT);
+		listTabName = new ArrayList<String>();
 	}
 
-	public void addTab(JPanel jPanel) {
-		// TODO thêm 1 tab moi
+	private boolean checkTabExist(String title) {
+		boolean flgExist = false;
+		for (int i = 0; i < listTabName.size(); i++) {
+			if (listTabName.get(i).equals(title)) {
+				flgExist = true;
+				break;
+			}
+		}
+
+		return flgExist;
 	}
 
-	public void removeTab() {
-		// TODO remove tab dang mo
+	@Override
+	public void addTab(String title, Component component) {
+		if (!checkTabExist(title)) {
+			super.addTab(title, component);
+			listTabName.add(title);
+			if (listTabName.size() != 1) {
+				setTabComponentAt(listTabName.size() - 1, new CustomTab(title));
+			}
+			setSelectedIndex(listTabName.size() - 1);
+		} else {
+			int index = -1;
+			for (int i = 0; i < listTabName.size(); i++) {
+				if (listTabName.get(i).equals(title)) {
+					index = i;
+					break;
+				}
+			}
+
+			if (index != -1) {
+				setSelectedIndex(index);
+			}
+		}
+	}
+
+	public void removeTab(String title) {
+		// TODO dung de remove tab voi title name tuong ung
+	}
+
+	private class CustomTab extends JPanel {
+
+		private static final long serialVersionUID = 1L;
+
+		private JLabel lblTitleTab;
+		private JButton btnCloseTab;
+
+		public CustomTab(String title) {
+			setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+			setOpaque(false);
+
+			lblTitleTab = new JLabel(title);
+			btnCloseTab = new JButton(new ImageIcon("image/close.png"));
+			btnCloseTab.setPreferredSize(new Dimension(15, 15));
+			btnCloseTab.setOpaque(false);
+
+			add(lblTitleTab);
+			add(btnCloseTab);
+		}
 	}
 }
